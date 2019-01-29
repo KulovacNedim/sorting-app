86acb1f9dbcc5e546bdd3bea2187ed2962a21fda
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-01-29 16:24:56 +0100

    Customers routing added

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -6,10 +6,19 @@ import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-text.component';
 import { SharedModule } from '../shared/shared.module';
+import { CustomersRoutingModule } from './customers-routing';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, FormsModule ],
-  declarations: [CustomersComponent, CustomersListComponent, FilterTextboxComponent],
-  exports: [ CustomersComponent ]
+  imports: [
+    CommonModule, 
+    SharedModule, 
+    FormsModule,
+    CustomersRoutingModule ],
+  declarations: [
+    CustomersComponent, 
+    CustomersListComponent, 
+    FilterTextboxComponent],
+  exports: [ 
+    CustomersComponent ]
 })
 export class CustomersModule { }
